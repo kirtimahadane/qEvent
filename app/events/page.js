deee77fffa7d1ac7627a7fa665c250b169a9ad24
kirtimahadane <kirti.mahadane@gmail.com>
2024-09-19 15:35:50 +0530

    module2 milestone 1 evets data ui

--- app/events/page.js
+++ app/events/page.js
@@ -1,7 +1,19 @@
-export default function EventsPage(){
+
+import EventCard from "@/components/EventCard";
+export default async function EventsPage(){
+   const response= await fetch("https://qevent-backend.labs.crio.do/events") ;
+   const events= await response.json();
+   
     return (
-        <>
-        <h1>Events Page</h1>
-        </>
+        
+        <div className="w-full flex flex-wrap">
+{events.map(event=>{
+            return(
+                <EventCard eventData={event}/>
+            )
+        })}
+        </div>
+        
+        
     )
 }
